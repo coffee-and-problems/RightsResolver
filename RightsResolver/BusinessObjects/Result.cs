11e4546c180adfd6b23303b7fe915e022102a0e8
Maria <marychobanu@gmail.com>
2019-11-13 17:23:48 +0300

    Add Result

---
+++ RightsResolver/BusinessObjects/Result.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace RightsResolver
+{
+    public class Result
+    {
+        public bool IsSuccessful { get; }
+        public string Message { get; }
+        public List<UserRights> Data { get; }
+
+        public Result(bool isSuccessful, string message = null, List<UserRights> data = null)
+        {
+            IsSuccessful = isSuccessful;
+            Message = message;
+            Data = data;
+        }
+
+        public static Result GenerateSuccess(List<UserRights> data)
+        {
+            return new Result(true, data: data);
+        }
+
+        public static Result GenerateFail(string message)
+        {
+            return new Result(false, message: message);
+        }
+    }
+}
